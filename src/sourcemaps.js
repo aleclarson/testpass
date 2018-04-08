c7e8e01b17f5123ae1237b1924f01f8022bebdce
aleclarson <alec.stanford.larson@gmail.com>
2018-04-08 17:14:15 -0400

    fix: disable `source-map-support` uncaught error shim
    We don't want the process to exit on uncaught errors, so tell source-map-support not to shim process.emit (we do that ourselves 😉)

--- src/sourcemaps.js
+++ src/sourcemaps.js
@@ -7,6 +7,7 @@ exports.enableInlineMaps = function() {
   // Hook into `Module._compile` for VM module support.
   sourceMapSupport.install({
     hookRequire: true,
+    handleUncaughtExceptions: false,
   })
 
   // Reset `Error.prepareStackTrace` because we use `wrapCallSite` directly.
